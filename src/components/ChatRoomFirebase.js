98e738aa3de3af013a53034fade2e3a93ace838e
Zi Nean Teoh <zineanteoh@Zi-N-Teohs-MacBook-Pro.local>
2021-10-09 18:57:57 -0500

    changed text to sign out

--- src/components/ChatRoomFirebase.js
+++ src/components/ChatRoomFirebase.js
@@ -60,7 +60,7 @@ function SignIn() {
 
 function SignOut() {
   return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Close</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 

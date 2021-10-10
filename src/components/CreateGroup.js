904307ddd2a9fa20a34588c074439a13b066773b
Zi Nean Teoh <zineanteoh@Zi-N-Teohs-MacBook-Pro.local>
2021-10-09 19:52:06 -0500

    Added popups

---
+++ src/components/CreateGroup.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./CreateGroup.css";
+
+class CreateGroup extends Component {
+	render() {
+		return (
+			<div className="creategroup">
+				<div className="title">
+					<h1>Create a New Group</h1>
+				</div>
+				<div class="location">
+					<div class="select">
+						<select name="slct" id="slct">
+							<option>Select a location</option>
+							<option value="1">Stevenson Library</option>
+							<option value="2">Central Library</option>
+							<option value="3">Stevenson 3</option>
+						</select>
+					</div>
+				</div>
+
+				<div class="partition anyone">
+					<h3>Study with Anyone</h3>
+					<div className="brown1_owl"></div>
+					<div className="yellow_owl"></div>
+					<div className="pink_owl"></div>
+				</div>
+
+				<div class="partition subject">
+					<h3>Study by Subject</h3>
+					<div className="brown1_owl"></div>
+					<div className="brown2_owl"></div>
+					<div className="brown3_owl"></div>
+					<div class="selectC">
+						<select name="slct" id="slct">
+							<option>Select a subject</option>
+							<option value="1">Biology</option>
+							<option value="2">Calculus</option>
+							<option value="3">Chemistry</option>
+							<option value="4">Cognitive Studies</option>
+							<option value="5">Computer Science</option>
+							<option value="6">Economics</option>
+							<option value="7">History</option>
+							<option value="8">Psychology</option>
+							<option value="9">Statistics</option>
+						</select>
+					</div>
+				</div>
+				<div class="submit">
+					<h4>Submit</h4>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default CreateGroup;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
 import "./Map.css";
+import CreateGroup from "./CreateGroup";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 class Map extends Component {
-    render() {
-        return (
-            <div className="map">
-                <div className="map-area"></div>
-                {/* <h2>Lobby</h2> */}
-            </div>
-        );
-    }
+
+	render() {
+		return (
+			<div className="map">
+				<div className="map-area">
+					<div className="corner-buttons">
+						<Popup
+							trigger={
+								<button className="create-group-button">
+									Create Group
+								</button>
+							}
+							modal
+							nested
+						>
+							<CreateGroup/>
+						</Popup>
+						<Popup
+							trigger={
+								<button className="join-group-button">
+									Join Group
+								</button>
+							}
+							modal
+							nested
+						>
+							<div>Popup content here !!</div>
+						</Popup>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
+
+// class Map extends Component {
+//     render() {
+//         return (
+//             <div className="map">
+//                 <div className="map-area"></div>
+//                 {/* <h2>Lobby</h2> */}
+//             </div>
+//         );
+//     }
+// }
 
 export default Map;
